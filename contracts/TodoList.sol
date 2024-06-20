940db435a80a0d325f686e3d39a8892963187c5d
Akshay <akshay@bpract.com>
2024-06-20 21:02:25 +0530

    some changes

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,18 +3,18 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "../interfaces/IERC20.sol";
 import "../interfaces/ITodoList.sol";
-import "truffle/Console.sol";
 
 contract TodoList is ITodoList {
-	address private owner;
-	uint public taskCount = 0;
+    address private owner;
+    uint public taskCount = 0;
     uint[] private availableTaskIds;
     mapping(uint => Task) public tasks;
     mapping(address => Task[]) private users;
 
     IERC20 private token;
-    uint private constant AMOUNT_FOR_NEW_USER  =  100;
+    uint private constant AMOUNT_FOR_NEW_USER = 100;
     uint private constant AMOUNT_FOR_NEW_TASK = 10;
+
     struct Task {
         uint id;
         string content;
@@ -31,48 +31,63 @@ contract TodoList is ITodoList {
     event UserAdded(address userAddress, string message);
 
     constructor() {
-    	owner = msg.sender;
+        owner = msg.sender;
     }
 
-    function setToken(address _token) public  {
-    	require(msg.sender == owner, "Get lost, I have nothing to do with you!!!");
-    	token = IERC20(_token);
+    function setToken(address _token) public {
+        require(
+            msg.sender == owner,
+            "Get lost, I have nothing to do with you!!!"
+        );
+        token = IERC20(_token);
     }
 
-    function getCurrentTokenHolding() public view returns(uint256) {
-    	return token.balanceOf(address(this));
+    function getCurrentTokenHolding() public view returns (uint256) {
+        return token.balanceOf(address(this));
     }
 
-
-    function connectUser() public returns(bool) {
-     	users[msg.sender];
-    	emit UserAdded(msg.sender, "User Added");
-     	token.transfer(msg.sender,AMOUNT_FOR_NEW_USER);
-      	emit TokenTransferred(AMOUNT_FOR_NEW_USER,"TT transfered", msg.sender);
-       	return true;
+    function connectUser() public returns (bool) {
+        users[msg.sender];
+        emit UserAdded(msg.sender, "User Added");
+        token.transfer(msg.sender, AMOUNT_FOR_NEW_USER);
+        emit TokenTransferred(AMOUNT_FOR_NEW_USER, "TT transfered", msg.sender);
+        return true;
     }
 
-    function getTaskLength() public view returns(uint) {
-    	return users[msg.sender].length;
+    function getTaskLength() public view returns (uint) {
+        return users[msg.sender].length;
     }
 
     function addTask(string memory _content, uint256 endTime) public {
-   		bool status  = token.transferFrom(msg.sender, address(this), AMOUNT_FOR_NEW_TASK);
-     	require(status == true, "Failed to deposit Tokens");
+        bool status = token.transferFrom(
+            msg.sender,
+            address(this),
+            AMOUNT_FOR_NEW_TASK
+        );
+        require(status == true, "Failed to deposit Tokens");
 
-    	uint myTasks = getTaskLength();
-    	Task memory task = Task(myTasks + 1, _content, false, block.timestamp, endTime);
-     	users[msg.sender].push(task);
-      	emit TaskAdded("A New Task has been added");
+        uint myTasks = getTaskLength();
+        Task memory task = Task(
+            myTasks + 1,
+            _content,
+            false,
+            block.timestamp,
+            endTime
+        );
+        users[msg.sender].push(task);
+        emit TaskAdded("A New Task has been added");
     }
 
-    function getTask(uint index) public view returns(Task memory){
-    	Task[] memory  myTasks = users[msg.sender];
-     	uint _taskCount = getTaskLength();
-      	require(index < _taskCount, 'Task with the specified index does not exist');
-     	return myTasks[index];
+    function getTask(uint index) public view returns (Task memory) {
+        Task[] memory myTasks = users[msg.sender];
+        uint _taskCount = getTaskLength();
+        require(
+            index < _taskCount,
+            "Task with the specified index does not exist"
+        );
+        return myTasks[index];
     }
-/*
+    /*
     function
     removeTask(uint index) public {
     	Task[] storage _tasks = users[msg.sender];
@@ -94,19 +109,25 @@ contract TodoList is ITodoList {
             emit Removed(_id, "Task removed");
         }
         */
-    function getPercentageOfTimeElapsed(uint256 startTime, uint256 endTime) private view  returns(uint256) {
-    	uint256 currentTime = block.timestamp;
-     	uint256 totalTime = endTime - startTime;
-      	uint256 elapsedTimeFromStartTime = currentTime - startTime;
-	    uint256 percentageOfTimeElapsed = (elapsedTimeFromStartTime / totalTime) * 100;
-		return percentageOfTimeElapsed;
+    function getPercentageOfTimeElapsed(
+        uint256 startTime,
+        uint256 endTime
+    ) private view returns (uint256) {
+        uint256 currentTime = block.timestamp;
+        uint256 totalTime = endTime - startTime;
+        uint256 elapsedTimeFromStartTime = currentTime - startTime;
+        uint256 percentageOfTimeElapsed = (elapsedTimeFromStartTime /
+            totalTime) * 100;
+        //return startTime - endTime;
+        return elapsedTimeFromStartTime;
     }
 
-    function markComplete(uint index) public returns(uint256) {
-	   	require(index < getTaskLength(), "Index Overshot");
-	    Task memory task = getTask(index);
-		return getPercentageOfTimeElapsed(task.startTime, task.endTime);
-/*
+    function markComplete(uint index) public returns (uint256) {
+        require(index < getTaskLength(), "Index Overshot");
+        Task memory task = getTask(index);
+        //return task.endTime;
+        return getPercentageOfTimeElapsed(task.startTime, task.endTime);
+        /*
 	    emit Toggling(task.id, task.content, task.completed);
 	    task.completed = !task.completed;
 	    emit Toggled(task.id, task.content, task.completed);*/
--- test/todo_list.js
+++ test/todo_list.js
@@ -1,79 +1,123 @@
 const TodoList = artifacts.require("TodoList");
-const Token = artifacts.require("TodoToken")
+const Token = artifacts.require("TodoToken");
 
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("TodoList", async function([primary]) {
-  it("Deployed successfully", async function() {
-    const todoList = await TodoList.deployed();
-    const token = await Token.deployed()
-    const balance = await token.balanceOf(todoList.address)
-    assert.equal(balance, '1000000000000000000000000', "Balance does not match")
-  });
+contract("TodoList", async function ([primary]) {
+    it("Deployed successfully", async function () {
+        const todoList = await TodoList.deployed();
+        const token = await Token.deployed();
+        const balance = await token.balanceOf(todoList.address);
+        assert.equal(
+            balance,
+            "1000000000000000000000000",
+            "Balance does not match"
+        );
+    });
 
-  it('User connected successfully', async function() {
-    const todoList = await TodoList.deployed();
-    const token = await Token.deployed()
-    const status = await todoList.connectUser()
-    const balance = await token.balanceOf(primary)
-    assert.strictEqual(balance.toString(), '100', 'Transferred Amount is not correct')
-  })
+    it("User connected successfully", async function () {
+        const todoList = await TodoList.deployed();
+        const token = await Token.deployed();
+        const status = await todoList.connectUser();
+        const balance = await token.balanceOf(primary);
+        assert.strictEqual(
+            balance.toString(),
+            "100",
+            "Transferred Amount is not correct"
+        );
+    });
 
-  it('Task added successfully', async function() {
-    const todoList = await TodoList.deployed();
-    const todo = await TodoList.deployed()
-    const token = await Token.deployed()
+    it("Task added successfully", async function () {
+        const todoList = await TodoList.deployed();
+        const todo = await TodoList.deployed();
+        const token = await Token.deployed();
 
-    await token.approve(todo.address, 10);
-    var date = new Date("07/14/2024 15:00:00");
+        await token.approve(todo.address, 10);
+        var date = new Date("06/22/2024 14:12:00");
 
-    await todoList.addTask('This is my first Task', date.getTime() / 1000);
-    const taskLength = await todoList.getTaskLength()
-    assert.strictEqual(taskLength.toString(), '1', "Task length should be one but do not match");
-  })
-  it('Task persists', async function() {
-    const todo = await TodoList.deployed()
-    const taskLength = await todo.getTaskLength()
-    assert.strictEqual(taskLength.toString(), '1', "Task length should be one but do not match");
-  })
+        await todoList.addTask("This is my first Task", date.getTime() / 1000);
+        const taskLength = await todoList.getTaskLength();
+        assert.strictEqual(
+            taskLength.toString(),
+            "1",
+            "Task length should be one but do not match"
+        );
+    });
+    it("Task persists", async function () {
+        const todo = await TodoList.deployed();
+        const taskLength = await todo.getTaskLength();
+        assert.strictEqual(
+            taskLength.toString(),
+            "1",
+            "Task length should be one but do not match"
+        );
+    });
 
-  it('Ten Tokens transferred to user', async function() {
-    const todo = await TodoList.deployed()
-    const token = await Token.deployed()
-    const taskLength = await todo.getTaskLength()
-    assert.strictEqual(taskLength.toString(), '1', "Task length should be one but do not match");
+    it("Ten Tokens transferred to user", async function () {
+        const todo = await TodoList.deployed();
+        const token = await Token.deployed();
+        const taskLength = await todo.getTaskLength();
+        assert.strictEqual(
+            taskLength.toString(),
+            "1",
+            "Task length should be one but do not match"
+        );
 
-    const balance = await token.balanceOf(primary)
-    assert.strictEqual(balance.toString(), '90', 'Transferred Amount is not correct')
-  })
+        const balance = await token.balanceOf(primary);
+        assert.strictEqual(
+            balance.toString(),
+            "90",
+            "Transferred Amount is not correct"
+        );
+    });
 
-  it('Reward calculated', async function() {
-    const todo = await TodoList.deployed()
-    const reward = (await todo.markComplete.call(0))
-    console.log(await reward.toString())
-    // assert.strictEqual(1, 1, 'test')
-  }) // it('Failed to remove task', async function() {
-  //   const todo = await TodoList.deployed()
-  //   const taskLength = await todo.getTaskLength()
-  //   console.log(taskLength.toString())
-  //   await todo.addTask('This is my first Task');
-  //   await todo.addTask('This is my first Task');
-  //   // await todo.removeTask(0);
-  //   assert.strictEqual(taskLength.toString(), '0', "Task length should be zero but do not match");
+    it("Reward calculated", async function () {
+        const todo = await TodoList.deployed();
+        function timeout(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms));
+        }
 
-  // })
-  // it('Task added succesfully', async function() {
-  //   const todoList = await TodoList.deployed();
-  //   await todoList.addTask('This is first Task');
-  //   const taskCount = await todoList.taskCount();
-  //   assert.strictEqual(taskCount.toString(), '1', `Task Count is ${taskCount.toString()} instead of '1'`);
-  // })
-  // it("Ten Tokens transferred to the task creator", async function() {
-  //   const token = await Token.deployed()
-  //   const balance = await token.balanceOf(primary)
-  //   assert.strictEqual(balance.toString(), '100', "Transferred Amount is not correct")
-  // })
+        console.log("10 second timer started");
+        await timeout(20000);
+        console.log("timeout ended");
+        const test = await todo.markComplete.call(0);
+        console.log(test.toString());
+
+        //const task = await todo.getTask(0);
+        //const startTime = task[3];
+        //const endTime = task[4];
+        //console.log(
+        //    startTime,
+        //    endTime,
+        //    startTime - endTime,
+        //    endTime - startTime
+        //);
+
+        //const reward = await todo.markComplete.call(0) // a transaction will not return a value but only a transaction hash
+        //console.log(await reward.toString())
+        // assert.strictEqual(1, 1, 'test')
+    }); // it('Failed to remove task', async function() {
+    //   const todo = await TodoList.deployed()
+    //   const taskLength = await todo.getTaskLength()
+    //   console.log(taskLength.toString())
+    //   await todo.addTask('This is my first Task');
+    //   await todo.addTask('This is my first Task');
+    //   // await todo.removeTask(0);
+    //   assert.strictEqual(taskLength.toString(), '0', "Task length should be zero but do not match");
+
+    // })
+    // it('Task added succesfully', async function() {
+    //   const todoList = await TodoList.deployed();
+    //   await todoList.addTask('This is first Task');
+    //   const taskCount = await todoList.taskCount();
+    //   assert.strictEqual(taskCount.toString(), '1', `Task Count is ${taskCount.toString()} instead of '1'`);
+    // })
+    // it("Ten Tokens transferred to the task creator", async function() {
+    //   const token = await Token.deployed()
+    //   const balance = await token.balanceOf(primary)
+    //   assert.strictEqual(balance.toString(), '100', "Transferred Amount is not correct")
+    // })
 });
